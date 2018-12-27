e89da7d282735650aecd020baf918864e6853510
Corentin BOISSON <corentin.boisson@devinci.fr>
2018-12-27 19:23:41 +0100

    Position

--- src/main/java/battleship/Position.java
+++ src/main/java/battleship/Position.java
@@ -2,57 +2,13 @@ package battleship;
 
 public class Position{
 
-    private int row;
-    private int column;
-    private boolean isOccupied;
-    private boolean isHit;
+    private boolean Présence_navire;
 
-    public Position(int row, int column, boolean isOccupied, boolean isHit) {
-        this.row = row;
-        this.column = column;
-        this.isOccupied = isOccupied;
-        this.isHit = isHit;
-    }
+    private boolean Navire_adversaire_touché;
+    private boolean Navire_adversaire_loupé;
 
-    public int getRow() {
-        return row;
-    }
+    private boolean Missile_adversaire_touché;
+    private boolean Missile_adversaire_loupé;
 
-    public void setRow(int row) {
-        this.row = row;
-    }
 
-    public int getColumn() {
-        return column;
-    }
-
-    public void setColumn(int column) {
-        this.column = column;
-    }
-
-    public boolean isOccupied() {
-        return isOccupied;
-    }
-
-    public void setOccupied(boolean occupied) {
-        isOccupied = occupied;
-    }
-
-    public boolean isHit() {
-        return isHit;
-    }
-
-    public void setHit(boolean hit) {
-        isHit = hit;
-    }
-
-    @java.lang.Override
-    public java.lang.String toString() {
-        return "Position{" +
-                "row=" + row +
-                ", column=" + column +
-                ", isOccupied=" + isOccupied +
-                ", isHit=" + isHit +
-                '}';
-    }
 }

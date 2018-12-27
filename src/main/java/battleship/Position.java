1b7aecdca7d3b40c8ba9c395462c713ca2d7f956
Corentin BOISSON <corentin.boisson@devinci.fr>
2018-12-27 19:25:20 +0100

    Position

--- src/main/java/battleship/Position.java
+++ src/main/java/battleship/Position.java
@@ -2,59 +2,59 @@ package battleship;
 
 public class Position{
 
-    private boolean Présence_navire;
+    private boolean Presence_navire;
 
-    private boolean Navire_adversaire_touché;
-    private boolean Navire_adversaire_loupé;
+    private boolean Navire_adversaire_touche;
+    private boolean Navire_adversaire_loupe;
 
-    private boolean Missile_adversaire_touché;
-    private boolean Missile_adversaire_loupé;
+    private boolean Missile_adversaire_touche;
+    private boolean Missile_adversaire_loupe;
 
-    public Position(boolean présence_navire, boolean navire_adversaire_touché, boolean navire_adversaire_loupé, boolean missile_adversaire_touché, boolean missile_adversaire_loupé) {
-        Présence_navire = présence_navire;
-        Navire_adversaire_touché = navire_adversaire_touché;
-        Navire_adversaire_loupé = navire_adversaire_loupé;
-        Missile_adversaire_touché = missile_adversaire_touché;
-        Missile_adversaire_loupé = missile_adversaire_loupé;
+    public Position(boolean presence_navire, boolean navire_adversaire_touche, boolean navire_adversaire_loupe, boolean missile_adversaire_touche, boolean missile_adversaire_loupe) {
+        Presence_navire = presence_navire;
+        Navire_adversaire_touche = navire_adversaire_touche;
+        Navire_adversaire_loupe = navire_adversaire_loupe;
+        Missile_adversaire_touche = missile_adversaire_touche;
+        Missile_adversaire_loupe = missile_adversaire_loupe;
     }
 
-    public boolean isPrésence_navire() {
-        return Présence_navire;
+    public boolean isPresence_navire() {
+        return Presence_navire;
     }
 
-    public void setPrésence_navire(boolean présence_navire) {
-        Présence_navire = présence_navire;
+    public void setPresence_navire(boolean presence_navire) {
+        Presence_navire = presence_navire;
     }
 
-    public boolean isNavire_adversaire_touché() {
-        return Navire_adversaire_touché;
+    public boolean isNavire_adversaire_touche() {
+        return Navire_adversaire_touche;
     }
 
-    public void setNavire_adversaire_touché(boolean navire_adversaire_touché) {
-        Navire_adversaire_touché = navire_adversaire_touché;
+    public void setNavire_adversaire_touche(boolean navire_adversaire_touche) {
+        Navire_adversaire_touche = navire_adversaire_touche;
     }
 
-    public boolean isNavire_adversaire_loupé() {
-        return Navire_adversaire_loupé;
+    public boolean isNavire_adversaire_loupe() {
+        return Navire_adversaire_loupe;
     }
 
-    public void setNavire_adversaire_loupé(boolean navire_adversaire_loupé) {
-        Navire_adversaire_loupé = navire_adversaire_loupé;
+    public void setNavire_adversaire_loupe(boolean navire_adversaire_loupe) {
+        Navire_adversaire_loupe = navire_adversaire_loupe;
     }
 
-    public boolean isMissile_adversaire_touché() {
-        return Missile_adversaire_touché;
+    public boolean isMissile_adversaire_touche() {
+        return Missile_adversaire_touche;
     }
 
-    public void setMissile_adversaire_touché(boolean missile_adversaire_touché) {
-        Missile_adversaire_touché = missile_adversaire_touché;
+    public void setMissile_adversaire_touche(boolean missile_adversaire_touche) {
+        Missile_adversaire_touche = missile_adversaire_touche;
     }
 
-    public boolean isMissile_adversaire_loupé() {
-        return Missile_adversaire_loupé;
+    public boolean isMissile_adversaire_loupe() {
+        return Missile_adversaire_loupe;
     }
 
-    public void setMissile_adversaire_loupé(boolean missile_adversaire_loupé) {
-        Missile_adversaire_loupé = missile_adversaire_loupé;
+    public void setMissile_adversaire_loupe(boolean missile_adversaire_loupe) {
+        Missile_adversaire_loupe = missile_adversaire_loupe;
     }
 }

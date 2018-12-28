bfec925d3b134723d41503faff6312534eab704a
Corentin BOISSON <corentin.boisson@devinci.fr>
2018-12-28 21:28:51 +0100

    Coordonnees

--- src/main/java/battleship/Main.java
+++ src/main/java/battleship/Main.java
@@ -5,28 +5,28 @@ import java.util.List;
 
 public class Main {
 
-    public int[] coordonnees(char colonne, int ligne, int altitude)
+    public int[] coordonnees(char colonne, int ligne)
     {
         int Ligne=ligne-1;
         int Colonne=-1;
         int code = Character.getNumericValue(colonne);
 
-        if (code >=65 && code <=90)
+        if (code >=65 && code <=75)
         {
         Colonne=code-65;
         }
 
         else
         {
-            if (code >=97 && code <=122)
+            if (code >=97 && code <=107)
             {
                 Colonne=code-97;
             }
         }
 
-        int Altitude = altitude + 4;
 
-        int Coordonnees[]  = {Colonne,Ligne,Altitude};
+
+        int Coordonnees[]  = {Colonne,Ligne};
 
         return(Coordonnees);
 
